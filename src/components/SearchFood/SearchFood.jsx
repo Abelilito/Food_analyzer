5495a7dc99127bc5fefdeb25103faee1a87a6398
Abel Ilito Limoma <ilitoabel@gmail.com>
2025-11-12 13:36:07 +0100

    Feat: install Tailwind

--- src/components/SearchFood/SearchFood.jsx
+++ src/components/SearchFood/SearchFood.jsx
@@ -36,11 +36,10 @@ export const SearchFood = () => {
     return <div>Erreur : que ce t'a fait</div>;
   }
 
-  // console.log(getFood.products[0].code);
-  console.log(getCode);
+  console.log(getFood);
 
   return (
-    <>
+    <div className="flex flex-col items-center w-full">
       <form action={onSubmit}>
         <input
           name="food_name"
@@ -50,32 +49,44 @@ export const SearchFood = () => {
         <button type="submit">Recherche</button>
       </form>
 
-      <div className="card-wrapper">
-        {searchText &&
-          getFood.products?.map((product, index) => (
+      <div className="w-full grid justify-center gap-4 grid-cols-[repeat(auto-fit,100%)] md:grid-cols-[repeat(auto-fit,40rem)]">
+        {searchText && getFood.products?.map((product, index) =>
+          product.product_name_fr && (
             <label
               htmlFor={product.id}
-              className="product-card"
+              className="
+                block box-content p-4 rounded-[10px] cursor-pointer border-2 border-solid border-transparent
+                has-[>input:checked]:border-[#E1DCCF]
+              "
               key={index}
               onClick={() => setGetCode(product.code)}
             >
-              <input type="checkbox" id={product.id} name={product.id} />
-              <div className="product-detail">
-                <div className="flex flex-row jc-center">
-                  <div className="img-wrapper">
-                    <img src={product.image_url} />
-                  </div>
-
-                  <div className="product-name">{product.product_name}</div>
+              <input
+                type="checkbox"
+                id={product.id}
+                name={product.id}
+                className="absolute hidden"
+              />
+              <div className="flex flex-row gap-8">
+                <div className="h-[100px] overflow-hidden flex">
+                  <img
+                    src={product.image_url}
+                    title={product.product_name_fr}
+                  />
                 </div>
 
-                <div>
+                <div className="flex flex-col justify-around">
+                  <div className="font-bold text-[15px]">
+                    { product.quantity_imported ? `${product.product_name_fr} - ${product.quantity_imported}` : product.product_name_fr}
+                  </div>
+
                   <Nutriscore nutriscore={product.nutrition_grades} />
                 </div>
               </div>
             </label>
-          ))}
+          )
+        )}
       </div>
-    </>
+    </div>
   );
 };
